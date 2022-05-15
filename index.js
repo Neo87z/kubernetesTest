1afa689e9fb5523f1421cb7d3fddaa9b8a37cb39
Neo87z <shehanhoradagoda12@gmail.com>
2022-05-14 23:34:21 -0230

    Test DOne OK

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello SLIIT updated from CICD!')
+  res.send('Hello SLIIT  from CICD!')
 })
 
 app.listen(port, () => {
